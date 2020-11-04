69853357484ea3316816b95d10dad4b5b82ce505
vikas-chdy <70101390+vikas-chdy@users.noreply.github.com>
2020-11-05 03:04:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+async function getQuote() {
+    const proxyUrl = 'https://cors-anywhere.herokupapp.com/'
+    const apiUrl  = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data=await response.json();
+        console.log(data);
+    } catch (error) {
+        getQuote();
+        console.log('whoops','no quote',error);
+    }
+    
+}
+
+// on load
+getQuote();
